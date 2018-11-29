c31191dad37571df8da3be22ef0e4102d1ceb1a7
jihed HAJJI <hajjijihed@gmail.com>
2018-11-29 15:36:28 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
-echo "this was modified by jihed to hookup trigger"
+echo "this was modified by jihed to hookup trigger 22222222222222222"
